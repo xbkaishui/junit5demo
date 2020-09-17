95fe2423c28fb72ca912b3d210320513b0e3dfbd
xbkaishui <xbkaishui@126.com>
2020-09-17 14:01:48 +0800

    add test for not modified

--- src/test/java/com/xbkaishui/bookstoread/BookShelfTest.java
+++ src/test/java/com/xbkaishui/bookstoread/BookShelfTest.java
@@ -24,4 +24,17 @@ class BookShelfTest {
         List<String> books  = shelf.books();
         assertEquals(2, books.size(), () -> "BookShelf should have two books.");
     }
+
+    @Test
+    void booksReturnedFromBookShelfIsImmutableForClient() {
+        BookShelf shelf = new BookShelf();
+        shelf.add("Effective Java", "Code Complete");
+        List<String> books = shelf.books();
+        try {
+            books.add("The Mythical Man-Month");
+            fail(() -> "Should not be able to add book to books");
+        } catch (Exception e) {
+            assertTrue(e instanceof UnsupportedOperationException, () -> "Should throw UnsupportedOperationException.");
+        }
+    }
 }
